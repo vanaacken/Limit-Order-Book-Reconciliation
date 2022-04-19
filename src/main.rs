7b00ac38fc7aa2c026495bf34cca3f548711d2c0
Niels Van aacken <nvan-aac@f0r5s7.codam.nl>
2022-04-19 14:41:41 +0200

    refactor + best price

--- src/main.rs
+++ src/main.rs
@@ -2,104 +2,165 @@
 use tungstenite::connect;
 use url::Url;
 use std::collections::BTreeMap;
-use serde_json::json;
-use float_cmp::ApproxEq;
-use float_ord::FloatOrd;
+use serde_json::{Value, json};
+// use float_cmp::ApproxEq;
+// use float_ord::FloatOrd;
 use ordered_float::OrderedFloat;
+// use std::cmp::Ordering;
+use tungstenite::client::AutoStream;
+use tungstenite::handshake::client::Response;
+use tungstenite::{WebSocket, Message};
 
 mod models;
 use crate::models::Order;
 use crate::models::BookStreamWrapper;
-use std::cmp::Ordering;
-
+use crate::models::LimitOrderBook;
 
 // use lobr::{print_map_values, price_key};
 
 pub static DERIBIT_WS_API: &str = "wss://www.deribit.com/ws/api/v2";
 pub static DERIBIT_WS_API_TESTNET: &str = "wss://test.deribit.com/ws/api/v2";
-
-pub fn price_key(f_price: f64) -> i64 {
-    (f_price * 100.0) as i64
-}
  
-pub fn print_map_values(map: &BTreeMap<OrderedFloat<f64>, f64>) {
+fn print_lob(lob: &LimitOrderBook) {
+    let best_ask = lob.asks.iter().next().unwrap();    
     println!(
-        "Map size: {}, lowest ask: --, Quantitiy: --",
-        map.len(),
-        // map.into_values();
+        "Best ask: {}, Quantity: {}",
+        best_ask.1.price,
+        best_ask.1.qty,
     );
+    let best_bid = lob.bids.iter().next_back().unwrap();
+    println!(
+        "Best bid: {}, Quantity{}",
+        best_bid.1.price,
+        best_bid.1.qty,
+    )
 }
 
-
-fn main() {
-
-    let ws_url = DERIBIT_WS_API_TESTNET;
-    let (mut socket, _) = connect(Url::parse(ws_url).unwrap()).expect("Can't connect");
-   
-
-
-    let mut ask_tree = BTreeMap::new();
-    
-
-    let json_data = json!({
+fn parse_json_data() -> Value {
+    json!({
         "jsonrpc": "2.0",
             "method": "public/subscribe",
             "id": "42",
             "params": {
                 "channels": [
                     "book.BTC-PERPETUAL.100ms"]}
-    });
-    
+    })
+}
+
+fn deribit_connect() -> (WebSocket<AutoStream>, Response) {
+    let ws_url = DERIBIT_WS_API_TESTNET;
+    connect(Url::parse(ws_url).unwrap()).expect("Can't connect")
+}
+
+fn send_subscribe_msg(mut socket: WebSocket<AutoStream>) -> WebSocket<AutoStream>{
+    let json_data = parse_json_data();
     let msg = tungstenite::Message::Text(json_data.to_string());
-
     let _ = socket.write_message(msg);
-    let received = socket.read_message().expect("Error reading message");
-    println!("received {:?}", received);
-    loop {
+    socket
+}
 
-        let msg_recv = socket.read_message().expect("Error reading message");
-        let msg_recv = match msg_recv {
-            tungstenite::Message::Text(s) => s,
-            _ => {
-                panic!("Error getting text");
-            }
-        };
-        let parsed: BookStreamWrapper = serde_json::from_str(&msg_recv).expect("Can't parse");
-        for i in 0..parsed.params.data.asks.len() {
-            let f_price = parsed.params.data.asks[i].1;
-            let f_quantity = parsed.params.data.asks[i].2;
-            match String::from(&parsed.params.data.asks[i].0).as_str() {
-                "new" => {
-                    ask_tree.insert(OrderedFloat(f_price), f_quantity);
-                }
-                "change" => {
-                    if let Some(order) = ask_tree.get_mut(&OrderedFloat(f_price)) {
-                        *order = f_quantity;
-                    }
-                }
-                "delete" => {
-                    ask_tree.remove(&OrderedFloat(f_price));
-                }
-                _ => ()
-            }
+fn subscription_response(mut socket: WebSocket<AutoStream>) -> (WebSocket<AutoStream>, Message) {
+    let recv = socket.read_message().expect("Error reading message");
+    (socket, recv)
+}
+
+fn read_subscription(mut socket: WebSocket<AutoStream>) -> (WebSocket<AutoStream>, BookStreamWrapper) {
+    let recv = socket.read_message().expect("Error reading message");
+    let recv = match recv {
+        tungstenite::Message::Text(s) => s,
+        _ => {
+            panic!("Error getting text");
         }
+    };
+    (
+        socket,
+        serde_json::from_str(&recv).expect("Can't parse")
+    )
+}
+
+fn match_asks(to_match: String, mut lob: LimitOrderBook, order: Order) -> LimitOrderBook {
+    match String::from(&to_match).as_str() {
+        "new" => {
+            lob = lob.asks_new(order)
+        }
+        "change" => {
+            lob = lob.asks_change(order)
+        }
+        "delete" => {
+            lob = lob.asks_delete(order)
+        }
+        _ => ()
+    };
+    lob
+}
+
+fn match_bids(to_match: String, mut lob: LimitOrderBook, order: Order) -> LimitOrderBook {
+    match String::from(&to_match).as_str() {
+        "new" => {
+            lob = lob.bids_new(order)
+        }
+        "change" => {
+            lob = lob.bids_change(order)
+        }
+        "delete" => {
+            lob = lob.bids_delete(order)
+        }
+        _ => ()
+    };
+    lob
+}
+
+fn resolve_deltas(mut lob: LimitOrderBook, parsed: &BookStreamWrapper) -> LimitOrderBook {
+    for i in 0..parsed.params.data.asks.len() {
+        let order = Order::new(
+            parsed.params.data.asks[i].1,
+            parsed.params.data.asks[i].2,
+        );
+        let to_match = String::from(&parsed.params.data.asks[i].0);
+        lob = match_asks(to_match, lob, order);
+    };
+    for i in 0..parsed.params.data.bids.len() {
+        let order = Order::new(
+            parsed.params.data.bids[i].1,
+            parsed.params.data.bids[i].2,
+        );
+        let to_match = String::from(&parsed.params.data.bids[i].0);
+        lob = match_bids(to_match, lob, order);
+    };
+    lob
+}
+
+
+// fn find_best_price(lob: LimitOrderBook) -> LimitOrderBook {
+
+// }
+
+
+fn main() {
+
+    let mut lob = LimitOrderBook::new();
+    
+    let mut socket: WebSocket<AutoStream>;
+    (socket, _) = deribit_connect();
+    socket = send_subscribe_msg(socket);
+    (socket, _) = subscription_response(socket);
+    loop {
+        let parsed: BookStreamWrapper;
+        (socket, parsed) = read_subscription(socket);
+        // TODO -----------------------------
+        //  Check Reconnection
+        //      -> renew connection
+        //      -> clear lob
+        // -----------------------------------
+        lob = resolve_deltas(lob, &parsed);
+        // TODO -----------------------------
+        //  find best bid/ask
+        // -----------------------------------
         println!("{}", parsed.params.data.timestamp);
-        print_map_values(&ask_tree);
-        // for i in 0..parsed.params.data.asks.len() {
-        //     println!(
-        //         "Delta: {}. ask: {}, size {}",
-        //         parsed.params.data.asks[i].0,
-        //         parsed.params.data.asks[i].1,
-        //         parsed.params.data.asks[i].2,
-        //     )
-        // }
-        // for i in 0..parsed.params.data.bids.len() {
-        //     println!(
-        //         "Delta: {}. bids: {}, size {}",
-        //         parsed.params.data.bids[i].0,
-        //         parsed.params.data.bids[i].1,
-        //         parsed.params.data.bids[i].2,
-        //     )
-        // }   
+
+        // TODO -----------------------------
+        //  Print Prices
+        // -----------------------------------
+        print_lob(&lob);
     }
 }
--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
+use ordered_float::OrderedFloat;
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 #[serde(rename_all = "lowercase")]
@@ -22,6 +23,7 @@ pub struct BookData {
     pub instrument_name: String,
     pub prev_change_id: Option<i64>,
     pub timestamp: u64,
+    pub r#type: String,
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
@@ -38,29 +40,112 @@ pub struct BookStreamWrapper {
     pub jsonrpc: String,
 }
 
+// #[derive(Debug, Clone, Copy)]
+// pub struct Key {
+//     pub key: OrderedFloat<f64>,
+// }
+
+// impl Key {
+//     pub fn new(price: f64) -> Self {
+//         let key = OrderedFloat(price)
+//         Key {key}
+//     }
+// }
+
 #[derive(Debug, Clone, Copy)]
 pub struct Order {
     pub price: f64,
-    pub size: f64,
+    pub qty: f64,
 }
-
 
 impl Order {
-    pub fn new(price: f64, size: f64) -> Self {
-        Order {price, size}
-    }
-}
-
-impl PartialEq for Order {
-    fn eq(&self, other: &Self) -> bool {
-        self.price == other.price
+    pub fn new(price: f64, qty: f64) -> Self {
+        Order {price, qty}
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct LimitOrderBook {
-    pub asks: BTreeMap<i64, Order>,
-    pub bids: BTreeMap<i64, Order>,
+    pub asks: BTreeMap<OrderedFloat<f64>, Order>,
+    pub bids: BTreeMap<OrderedFloat<f64>, Order>,
     pub best_ask: Order,
     pub best_bid: Order,
 }
+
+impl LimitOrderBook {
+    pub fn new() -> Self {
+        LimitOrderBook {
+            asks: BTreeMap::new(),
+            bids: BTreeMap::new(),
+            best_ask: Order::new(0.0, 0.0),
+            best_bid: Order::new(0.0, 0.0),
+        }
+    }
+
+
+    pub fn asks_new (mut self, order: Order) -> Self {
+        self.asks.insert(OrderedFloat(order.price), order);
+        self
+    }
+
+    pub fn asks_delete(mut self, order: Order) -> Self {
+        self.asks.remove(&OrderedFloat(order.price));
+        self
+    }
+
+    pub fn asks_change(mut self, order: Order) -> Self {
+        if let Some(item) = self.asks.get_mut(&OrderedFloat(order.price)) {
+            *item = order;
+        }
+        self
+    }
+
+    pub fn bids_new (mut self, order: Order) -> Self {
+        self.bids.insert(OrderedFloat(order.price), order);
+        self
+    }
+
+    pub fn bids_delete(mut self, order: Order) -> Self {
+        self.bids.remove(&OrderedFloat(order.price));
+        self
+    }
+
+    pub fn bids_change(mut self, order: Order) -> Self {
+        if let Some(item) = self.bids.get_mut(&OrderedFloat(order.price)) {
+            *item = order;
+        }
+        self
+    }
+}
+
+// #[derive(Debug, Copy, Clone)]
+// pub struct ChangeId{
+//     pub id: i64,
+//     pub prev_id: i64,
+// }
+
+// impl ChangeId {
+//     pub fn new(id: i64, )-> Self {
+//         let prev_id = id;
+//         ChangeId{id, prev_id}
+//     }
+
+//     pub 
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
